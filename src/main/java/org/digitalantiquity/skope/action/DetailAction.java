4657f8cdead17e710d0a18f0d67a2bbdc564bf78
Adam Brin <abrin@digitalantiquity.org>
2015-09-30 11:42:36 -0700

    forgot to uncomment for testing

--- src/main/java/org/digitalantiquity/skope/action/DetailAction.java
+++ src/main/java/org/digitalantiquity/skope/action/DetailAction.java
@@ -72,8 +72,8 @@ public class DetailAction extends ActionSupport {
             logger.debug(String.format("m: %s start (%s,%s) x(%s,%s) %s %s ", mode, x1, y1, x2, y2, cols, zoom));
             Map<String, String[]> list = luceneService.getDetails(x1, y2);
             logger.debug("done request");
-//            geoTiffService.getBandData(y1, x1);
-//            logger.debug("done2");
+            geoTiffService.getBandData(y1, x1);
+            logger.debug("done2");
             json = new ObjectMapper().writeValueAsString(list);
             stream = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
